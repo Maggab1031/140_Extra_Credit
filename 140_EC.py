4c463b0d9bc576a8a219058b7afb1f420b89ed1f
Gabe Magee <glma2016@pomona.edu>
2019-04-30 01:10:54 -0700

    da

--- 140_EC.py
+++ 140_EC.py
@@ -1,6 +1,9 @@
 import os
-"""
+import random
+import time
+import matplotlib.pyplot as plt
 
+"""
 Create a program to investigate any of the following:
 
 1. For what values of n is insertion sort faster than mergesort?
@@ -17,18 +20,99 @@ n's and an explanation of which base appears to be the best.
 4. I will award extra credit for investigating at most two of the topics.
 5. To submit, create a github, gitlab, bitbucket, or similar public repository and send me a link via email. Your repository should include both your code and the write up.
 6. Submissions should be sent in on or before May 8th (the last day of class).
-
-
 """
-class Template(object):
 
-    def __init__(self):
-        self.a = 1
+#inclusive for floor and ceiling
+def random_list_of_ints(size,floor,ceiling):
+    l = []
+    for i in range(0,size):
+        l.append(random.randint(floor,ceiling))
+    return l
+
+class Sort():
+
+    def sort(list):
+        return list
+
+class Mergesort(Sort):
+
+    def sort(lst):
+        l = lst.copy()
+        n = len(lst)
+        if n  == 1:
+            return l
+        elif n == 0:
+            return l
+        else:
+            mid = n//2
+            left = Mergesort.sort(l[:mid])
+            right = Mergesort.sort(l[mid:])
+            l = []
+            while(len(left)>0 and len(right)>0):
+                if left[0]>right[0]:
+                    l.append(right[0])
+                    del right[0]
+                else:
+                    l.append(left[0])
+                    del left[0]
+            while(len(right)>0):
+                l.append(right[0])
+                del right[0]
+            while(len(left)>0):
+                l.append(left[0])
+                del left[0]
+            return l
+
+class Insertion_Sort(Sort):
+
+    def sort(list):
+        l = list.copy()
+        res = [l[0]]
+        for i in l[1:]:
+            index = 0
+            j=0
+            while j<len(res) and i>res[j]:
+                j = j + 1
+            res = res[:j]+[i]+res[j:]
+        return res
+
+class Radix_Sort():
+
+    def sort(list):
+        return list
 
 
 def main():
-    a = Template()
-    print(a.a)
+    max = 1000
+    max_length = 12
+    iterations = 100
+    d = {}
+    d["merge"] = {}
+    d["insertion"] = {}
+    for i in range(max_length):
+        n = 2**i
+        print(n)
+        total_n_merge = 0.0
+        total_n_insertion = 0.0
+        for i in range(iterations):
+            lst = random_list_of_ints(n,0,max)
+            start = time.time()
+            Insertion_Sort.sort(lst)
+            total = time.time() - start
+            total_n_insertion = total_n_insertion + total
+
+            start = time.time()
+            Mergesort.sort(lst)
+            total = time.time() - start
+            total_n_merge = total_n_merge + total
+        d["merge"][n] = total_n_merge/iterations
+        d["insertion"][n] = total_n_insertion/iterations
+    for i in range(max_length):
+        n = 2**i
+        print(n)
+        print("merge:",d["merge"][n])
+        print("insertion:",d["insertion"][n])
+    return d
 
 if __name__ == '__main__':
     main()
